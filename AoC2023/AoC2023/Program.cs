205b3f68bfe8dc16a3cc8cc7308a4016c186386a
jaess105 <jaess105@uni-duesseldorf.de>
2023-12-06 08:12:04 +0100

    Updated Program run

--- AoC2023/AoC2023/Program.cs
+++ AoC2023/AoC2023/Program.cs
@@ -5,12 +5,14 @@ using AoC2023;
 const string day3Path = @"/home/jannik/proj/prog/aoc/aoc-2023/AoC2023/AoC2023/resources/day3_input.txt";
 const string day4Path = @"/home/jannik/proj/prog/aoc/aoc-2023/AoC2023/AoC2023/resources/day4_input.txt";
 const string day5Path = @"/home/jannik/proj/prog/aoc/aoc-2023/AoC2023/AoC2023/resources/day5_input.txt";
+const string day6Path = @"/home/jannik/proj/prog/aoc/aoc-2023/AoC2023/AoC2023/resources/day6_input.txt";
 
 var arr = new (DaySolution, string)[]
 {
-    (new Day3(), day3Path),
-    (new Day4(), day4Path),
-    (new Day5(), day5Path),
+    // (new Day3(), day3Path),
+    // (new Day4(), day4Path),
+    // (new Day5(), day5Path),
+    (new Day6(), day6Path)
 };
 
 foreach (var (solution, path) in arr)
